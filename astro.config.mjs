52a5403f575afe231a00ad688f52279255416814
nabilrn <2211522018_nabil@student.unand.ac.id>
2026-01-20 10:47:49 +0700

    Add custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nabilrn.github.io',
+    site: 'https://nabilrizkinavisa.me',
     base: '/',
     integrations: [sitemap()],
     build: {
